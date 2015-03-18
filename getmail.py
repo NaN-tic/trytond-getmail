877f62e792092bc7efe9d13446261f6232c27e3c
resteve <resteve@zikzakmedia.com>
2015-03-18 14:28:16 +0100

    pyflakes

--- getmail.py
+++ getmail.py
@@ -25,53 +25,53 @@ class GetmailServer(ModelSQL, ModelView):
     __name__ = 'getmail.server'
     name = fields.Char('Name', required=True)
     active = fields.Boolean('Active', states={
-        'readonly': Not(Equal(Eval('state'), 'draft')),
+            'readonly': Not(Equal(Eval('state'), 'draft')),
         }, depends=['state'])
     state = fields.Selection([
-        ('draft', 'Draft'),
-        ('done', 'Done'),
+            ('draft', 'Draft'),
+            ('done', 'Done'),
         ], 'State', readonly=True)
     server = fields.Char('Server', required=True, states={
-        'readonly': Not(Equal(Eval('state'), 'draft')),
+            'readonly': Not(Equal(Eval('state'), 'draft')),
         }, depends=['state'])
     port = fields.Integer('Port', required=True, states={
-        'readonly': Not(Equal(Eval('state'), 'draft')),
+            'readonly': Not(Equal(Eval('state'), 'draft')),
         }, depends=['state'])
     folder = fields.Char('Folder', states={
-        'readonly': Not(Equal(Eval('state'), 'draft')),
+            'readonly': Not(Equal(Eval('state'), 'draft')),
         }, depends=['state'])
     limit = fields.Integer('Limit', states={
-        'readonly': Not(Equal(Eval('state'), 'draft')),
+            'readonly': Not(Equal(Eval('state'), 'draft')),
         }, depends=['state'],
         help='Total emails by connection. Default is 10')
     timeout = fields.Integer('Time Out', states={
-        'readonly': Not(Equal(Eval('state'), 'draft')),
+            'readonly': Not(Equal(Eval('state'), 'draft')),
         }, depends=['state'],
         help='Default is 15')
     type = fields.Selection([
-#       ('pop', 'POP Server'),
-        ('imap', 'IMAP Server')
+            #('pop', 'POP Server'),
+            ('imap', 'IMAP Server')
         ], 'Server Type', required=True, states={
         'readonly': Not(Equal(Eval('state'), 'draft')),
         }, depends=['state'])
     ssl = fields.Boolean('SSL', states={
-        'readonly': Not(Equal(Eval('state'), 'draft')),
+            'readonly': Not(Equal(Eval('state'), 'draft')),
         }, depends=['state'])
     attachment = fields.Boolean('Add Attachments',
         help='Fetches mail with attachments if true.')
     username = fields.Char('User Name', required=True, states={
-        'readonly': Not(Equal(Eval('state'), 'draft')),
+            'readonly': Not(Equal(Eval('state'), 'draft')),
         }, depends=['state'])
     password = fields.Char('Password', required=True, states={
-        'readonly': Not(Equal(Eval('state'), 'draft')),
+            'readonly': Not(Equal(Eval('state'), 'draft')),
         }, depends=['state'])
     note = fields.Text('Description')
     model = fields.Many2One('ir.model', 'Model', required=True, states={
-        'readonly': Not(Equal(Eval('state'), 'draft')),
+            'readonly': Not(Equal(Eval('state'), 'draft')),
         }, depends=['state'],
         help='Select a model have getmail method.')
     priority = fields.Integer('Server Priority', states={
-        'readonly': Not(Equal(Eval('state'), 'draft')),
+            'readonly': Not(Equal(Eval('state'), 'draft')),
         }, depends=['state'],
         help='Priority between 0 to 10, define the order of Processing')
 
