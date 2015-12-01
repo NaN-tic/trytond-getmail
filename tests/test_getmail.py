9f2aa4ee082ed51f3ccb1c053e81437f87709d31
resteve <resteve@zikzakmedia.com>
2015-12-01 11:08:53 +0100

    Tests

--- tests/test_getmail.py
+++ tests/test_getmail.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part getmail module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the getmail module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class GetmailTestCase(unittest.TestCase):
+class GetmailTestCase(ModuleTestCase):
     'Test Getmail module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('getmail')
-
-    def test0005views(self):
-        'Test views'
-        test_view('getmail')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'getmail'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         GetmailTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
