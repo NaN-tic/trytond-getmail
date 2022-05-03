f9fcdb4096f46b44ff36a2b19ad1aad04e86d9b1
Albert Cervera i Areny <albert@nan-tic.com>
2022-05-03 18:37:21 +0200

    Remove CompanyTestMixin.

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,12 +1,9 @@
-
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
-from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class GetmailTestCase(CompanyTestMixin, ModuleTestCase):
+class GetmailTestCase(ModuleTestCase):
     'Test Getmail module'
     module = 'getmail'
 
